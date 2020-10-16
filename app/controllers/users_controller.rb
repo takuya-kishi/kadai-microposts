dec4350f17e5e74fc5121e35eb15159da900a8c4
takuya kishi <takuyak12@outlook.jp>
2020-10-16 07:07:16 +0000

    favorite_display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def likes
     @user = User.find(params[:id])
     @likes = @user.likes.page(params[:page])
-    
+    counts(@user)
   end
 
 private
